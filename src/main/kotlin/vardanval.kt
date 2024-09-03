d5c25f42cef8ea3b1074dfec88b5f3eaaf1a8de5
WIN 10 <setiawanmfikri@gmail.com>
2024-09-03 13:26:05 +0700

    add variabel.kt

---
+++ src/main/kotlin/vardanval.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    var varVariabel : Int
+    varVariabel =56 //assigning first vakue to varVariabel
+    println("Pemberian nilai pertama pada varVariabel ="+varVariabel)
+    varVariabel = 78 //assigning second value to variabel
+    println("Pemberian nilai kedua pada variabel ="+varVariabel)
+
+}
