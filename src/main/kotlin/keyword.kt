7b2a3ef7989af7034f2b35e838b639cbae69c083
WIN 10 <setiawanmfikri@gmail.com>
2024-09-03 12:14:43 +0700

    add Keyword.kt

---
+++ src/main/kotlin/keyword.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    println("Hello,ini program pertama saya di kotlin!")
+    //contoh komentar satu baris
+    /* contoh komentar */
+    /**
+     * kotlin Doc -->baris ini untuk mendokumentasikan program
+     */
+}
