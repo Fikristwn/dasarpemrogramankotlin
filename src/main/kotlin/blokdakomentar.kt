d32df7b9153d538866ff874dba1fdb3bf962f832
WIN 10 <setiawanmfikri@gmail.com>
2024-09-03 14:01:18 +0700

    add blokdakomentar.kt

---
+++ src/main/kotlin/blokdakomentar.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    println("hello,ini program pertama saya di kotlin")
+    //contoh komentar satu baris
+    /*
+
+    contoh komentar multi baris
+     */
+    /**
+     * kotlin Doc -->Baris Ini untuk mendokumentasikan kode program
+     */
+}
